891c7821db7e1d18dff5759c70668ab739f590e0
Daniel Brooks <bass_rock@me.com>
2023-07-31 08:29:50 -0700

    feat(analytics): adding in plugin setup

--- modules/glean/runtime/glean-plugin.client.ts
+++ modules/glean/runtime/glean-plugin.client.ts
@@ -1,10 +1,21 @@
 import Glean from '@mozilla/glean/web'
+import * as log from 'tauri-plugin-log-api'
 import { page } from '../../../telemetry/generated/pings'
 
+// MDN Code for reference https://github.com/mdn/yari/blob/d11b0a8ba59d6c68e763eb8922ed7ce12fef2cae/client/src/telemetry/glean-context.tsx#L100
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.hook('page:start', () => {
+  nuxtApp.hook('app:mounted', () => {
+    log.info('Glean: App Mounted, start initing glean')
+
     // TODO: add in client id, logging on dev, upload off on dev, page path params to the ping/metric
     Glean.initialize('mozilla-social.elk', true, {})
+    Glean.setLogPings(true)
+  })
+
+  nuxtApp.hook('page:finish', () => {
+    log.info('Glean: Page finished, sending page event')
+
+    // TODO: add in client id, logging on dev, upload off on dev, page path params to the ping/metric
     page.submit()
   })
 })
