5459aed5ba617d258020f5b406df24a5ebba1041
Aly Fluckey <afluckey@mozilla.com>
2023-10-11 15:22:00 -0400

    fix(glean): remove ui_additional_detail

--- telemetry/engagementDetails.ts
+++ telemetry/engagementDetails.ts
@@ -8,14 +8,11 @@ interface EngagementDetails {
 export const engagementDetails: EngagementDetails = {
   'settings.interface.colorMode': {
     engagement_type: 'general',
-    ui_additional_detail: 'Fired when a user changes their interface color mode',
   },
   'settings.interface.fontSize': {
     engagement_type: 'general',
-    ui_additional_detail: 'Fired when a user changes their interface font size',
   },
   'settings.interface.themeColor': {
     engagement_type: 'general',
-    ui_additional_detail: 'Fired when a user changes their interface theme color',
   },
 }
