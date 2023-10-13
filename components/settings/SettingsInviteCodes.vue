3c06afa11ea7ef65284a02f148d9e954d4e6303d
Daniel Brooks <bass_rock@me.com>
2023-10-12 18:32:13 -0700

    fix(codes): fixing code display to api (#63)

--- components/settings/SettingsInviteCodes.vue
+++ components/settings/SettingsInviteCodes.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
-const invites: string[] = await $fetch(`/api/${publicServer.value}/invites`)
+const user = currentUser.value
 
+interface InviteCode {
+  token: string
+  assigned?: string
+  assigned_at?: string
+  created_at: string
+}
+
+interface Invite {
+  tokens: InviteCode[]
+}
+
+const invite: Invite = await $fetch(`/api/${publicServer.value}/invites`, { headers: { authorization: `Bearer ${user?.token}` } })
 const { text, copy, copied } = useClipboard()
 
 const { t } = useI18n()
 
-const noInvites = invites.length === 0
+const noInvites = invite.tokens.length === 0
 const heading = noInvites ? t('invites.no_codes.title') : t('invites.title')
 const description = noInvites ? t('invites.no_codes.subtitle') : t('invites.subtitle')
 </script>
@@ -21,12 +33,12 @@ const description = noInvites ? t('invites.no_codes.subtitle') : t('invites.subt
       </p>
     </div>
     <main>
-      <div v-for="code in invites" :key="code" b-t-1px>
+      <div v-for="code in invite.tokens" :key="code.token" b-t-1px>
         <div flex justify-between items-center b-b-1px p-4 m-b--1px>
-          <span>{{ code }}</span>
+          <span>{{ code.token }}</span>
           <div>
-            <button flex justify-between items-center @click="copy(code)">
-              <span v-if="copied && text === code" m-r-2 bg-primary text-base-light p-x-3 b-rd-10>{{ $t("invites.copied") }}</span>
+            <button flex justify-between items-center @click="copy(code.token)">
+              <span v-if="copied && text === code.token" m-r-2 bg-primary text-base-light p-x-3 b-rd-10>{{ $t("invites.copied") }}</span>
               <div i-ri:file-copy-line />
             </button>
           </div>
--- server/api/[server]/invites.ts
+++ server/api/[server]/invites.ts
@@ -2,10 +2,11 @@ export default defineEventHandler(async (event) => {
   try {
     const { server } = getRouterParams(event)
 
-    const token = getCookie(event, 'accessToken')
+    const auth: string = getHeader(event, 'Authorization') as string
+
     const invites = await fetch(
       `https://${server}/api/invitation/v1/status`,
-      { headers: { authorization: `Bearer ${token}` } },
+      { headers: { authorization: auth } },
     )
       .then(response => response.json())
     return invites
