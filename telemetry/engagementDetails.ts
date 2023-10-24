03d81d65a459c463791c62f24ac4351197ef0595
Anthony Liddle <anthonyliddle@gmail.com>
2023-10-24 13:22:08 -0700

    fix(discover): add impression, click, and share events

--- telemetry/engagementDetails.ts
+++ telemetry/engagementDetails.ts
@@ -15,4 +15,10 @@ export const engagementDetails: EngagementDetails = {
   'settings.interface.themeColor': {
     engagement_type: 'general',
   },
+  'discover.recommendation.open': {
+    engagement_type: 'general',
+  },
+  'discover.recommendation.share': {
+    engagement_type: 'general',
+  },
 }
