e8a6aa1948a51d2da07e22489ed982f2546f62d5
Joseph Perez <joperez@dons.usfca.edu>
2023-10-30 17:05:25 -0700

    fix(posts): whoops, missed engagement_type on bookmarks feed, favorites feed, local feed, and federated feed

--- telemetry/engagementPostEvents.js
+++ telemetry/engagementPostEvents.js
@@ -31,4 +31,68 @@ export const postEvents = {
   'home.feed.post.report': {
     engagement_type: 'general',
   },
+  // Favorites feed
+  'favorites.feed.post.reply': {
+    engagement_type: 'reply',
+  },
+  'favorites.feed.post.reblog': {
+    engagement_type: 'boost',
+  },
+  'favorites.feed.post.favorite': {
+    engagement_type: 'favorite',
+  },
+  'favorites.feed.post.bookmark': {
+    engagement_type: 'bookmark',
+  },
+  'favorites.feed.post.report': {
+    engagement_type: 'general',
+  },
+  // Bookmarks feed
+  'bookmarks.feed.post.reply': {
+    engagement_type: 'reply',
+  },
+  'bookmarks.feed.post.reblog': {
+    engagement_type: 'boost',
+  },
+  'bookmarks.feed.post.favorite': {
+    engagement_type: 'favorite',
+  },
+  'bookmarks.feed.post.bookmark': {
+    engagement_type: 'bookmark',
+  },
+  'bookmarks.feed.post.report': {
+    engagement_type: 'general',
+  },
+  // Local feed
+  'local.feed.post.reply': {
+    engagement_type: 'reply',
+  },
+  'local.feed.post.reblog': {
+    engagement_type: 'boost',
+  },
+  'local.feed.post.favorite': {
+    engagement_type: 'favorite',
+  },
+  'local.feed.post.bookmark': {
+    engagement_type: 'bookmark',
+  },
+  'local.feed.post.report': {
+    engagement_type: 'general',
+  },
+  // Federated feed
+  'federated.feed.post.reply': {
+    engagement_type: 'reply',
+  },
+  'federated.feed.post.reblog': {
+    engagement_type: 'boost',
+  },
+  'federated.feed.post.favorite': {
+    engagement_type: 'favorite',
+  },
+  'federated.feed.post.bookmark': {
+    engagement_type: 'bookmark',
+  },
+  'federated.feed.post.report': {
+    engagement_type: 'general',
+  },
 }
